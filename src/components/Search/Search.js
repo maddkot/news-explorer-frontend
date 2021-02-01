36b91e470a63d1883c2c3dcbe61ecfb2799cad05
Rengevich Sergei <ser-rengevich@yandex.ru>
2021-02-01 21:06:48 +0300

    level-3(9round)

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -26,7 +26,7 @@ function Search({searchNews}) {
     return (
         <section className="search">
             <div className="search__container">
-            <h1 className="search__title">Что твориться в мире?</h1>
+            <h1 className="search__title">Что творится в мире?</h1>
             <p className="search__subtitle">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
             <form className="search__form" onSubmit={handleSubmit} noValidate>
                 <input className="search__input"
