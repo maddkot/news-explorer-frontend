62b38b304b810680b16ddd7603d7aa52e8c89d4b
Rengevich Sergei <ser-rengevich@yandex.ru>
2021-02-01 17:29:12 +0300

    level-3(8round)

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
 const API_KEY = 'd6950557b87c4d5eadd5860a779009e4';
-const PROXY_URL = 'https:nomoreparties.co/news/v2/everything';
+const PROXY_URL = 'https://nomoreparties.co/news/v2/everything';
 const DECREASE_DAY = 7;
 const NEWS_SHOWED = 30;
 
